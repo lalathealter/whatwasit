ea377dae84a8d825a98f801d093bc70ccbd989e8
lalathealter <musin.daniiil@yandex.ru>
2023-05-08 14:50:14 +0500

    added stubs for all three methods of get, del and set; Added a simple stub for localization

--- main.go
+++ main.go
@@ -33,14 +33,63 @@ func main() {
 		return
 	}
 
-	b.Handle("/hello", func(c tele.Context) error {
-		return c.Send("Hello!")
-	})
+	b.Handle("/start", welcomeHandler)
+	b.Handle("/set", setHandler)
+	b.Handle("/get", getHandler)
+	b.Handle("/del", delHandler)
 
 	fmt.Println("Starting the bot...")
 	b.Start()
 }
 
+var (
+	msgWelcome = `
+		HELLO
+	`
+)
+
+var messageTemplateMap = map[string]map[string]string{
+	"en": {
+		"hello": "Welcome to the WhatWasIt — a telegram bot that helps you to save and remember your login data!",
+		"set": "Login data for %s was set successfully",
+		"get": "Login: %s\nPassword: %s",
+		"del": "Your login data for %s was deleted successfully",
+	},
+}
+
+func getMessage(c tele.Context, msg string) string {
+	lang, ok := c.Get("lang").(string)
+	if !ok {
+		lang = "en"
+		c.Set("lang", lang)
+	}
+	message, ok := messageTemplateMap[lang][msg]
+	if !ok {
+		if lang == "" {
+			message = "error; the set language isn't supported"
+		} 
+		if msg == "" {
+			message = "error; the message can't be found"
+		} 
+	}
+	return message  
+}
+
+func welcomeHandler(c tele.Context) error {
+	return c.Send(getMessage(c, "hello"))
+}
+
+func setHandler(c tele.Context) error {
+	return c.Send(getMessage(c, "set"))
+}
+
+func getHandler(c tele.Context) error {
+	return c.Send(getMessage(c, "get"))
+}
+
+func delHandler(c tele.Context) error {
+	return c.Send(getMessage(c, "del"))
+}
 
 func MustGetEnv(key string) string {
 	envVal, ok := os.LookupEnv(key)
