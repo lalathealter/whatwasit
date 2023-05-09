509dd1f6aba852df4f6cd84244c7bcc5cf0d3a61
lalathealter <musin.daniiil@yandex.ru>
2023-05-09 17:34:42 +0500

    added to the docs an explanation on how to use spaces in logins, names and passwords

--- local/local.go
+++ local/local.go
@@ -9,7 +9,14 @@ import (
 
 var messageTemplateMap = map[string]map[string]string{
 	"en": {
-		"hello": "Welcome to the WhatWasIt — a telegram bot that helps you to save and remember your login data!",
+		"hello": `Welcome to the WhatWasIt — a telegram bot that helps you to save and remember your login data!
+/set service_name login pass — set or update your login data for a service
+/get service_name — get your login data for the service
+/del service_name — delete your login data for a service 
+/lang lang_tag — switch languages (available: en, ru)
+/help, /doc, /start — show this message
+Note: if you don't use or update your login data in 15 days, it's going to be deleted;
+If you want to use spaces in your names, logins or passwords, enclose them in double quotes like this: "my service name"`,
 		"set": "Login data for %s was set successfully",
 		"err-few-args": "error: not enough arguments provided",
 		"err-empty-arg": "error: empty arguments aren't allowed",
@@ -24,7 +31,14 @@ var messageTemplateMap = map[string]map[string]string{
 		"lang-err-no-message": "error; the response message can't be found",
 	},
 	"ru":  {
-		"hello": "Привет, меня зовут WhatWasIt — я Телеграм-бот который поможет вам сохранять и помнить свои пароли!",
+		"hello": `Привет, меня зовут WhatWasIt — я Телеграм-бот который поможет вам сохранять и помнить свои пароли!
+/set имя_сервиса логин пароль — установить или перезаписать данные для входа в сервис
+/get имя_сервиса — получить данные для входа в сервис
+/del имя_сервиса — удалить данные для входа в сервис
+/lang сокращение_языка — сменить язык (доступны: en, ru)
+/help, /doc, /start — вывести эту справку
+Внимание: если вы ни разу за 15 дней не запросите или не обновите данные для входа в сервис, то они будут удалены;
+Если вы хотите использовать пробелы в ваших именах для сервисов, паролях или логинах, то окружите их двойными кавычками, вот так: "моё имя для сервиса"`,
 		"set": "Данные входа для %s были сохранены успешно",
 		"err-few-args": "ошибка: недостаточно аргументов",
 		"err-empty-arg": "ошибка: пустые аргументы не допускаются",
